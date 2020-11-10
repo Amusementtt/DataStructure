4179caa844447a747db147bb29fbb60b42c20e27
wangyu <wangyu9@bonc.com.cn>
2020-11-10 23:35:13 +0800

    springboot日志系统新建项目提交

---
+++ src/main/java/com/wangyu/log/LogApplication.java
@@ -0,0 +1,16 @@
+package com.wangyu.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApplication {
+
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogApplication.class, args);
+		log.error("这是王钰的信息");
+	}
+
+}
